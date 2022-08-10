449353846f671f94f54dc5c91da53db38d2b2140
siham <phenissia2009@hotmail.fr>
2022-08-10 13:40:06 +0200

    console.py

--- console.py
+++ console.py
@@ -40,39 +40,39 @@ class HBNBCommand(cmd.Cmd):
         print("")
         return True
 
-    def do_create(self, line):
-        """Usage: create <class> <key 1>=<value 2> <key 2>=<value 2> ...
-        Create a new class instance with given keys/values and print its id.
-        """
-        try:
-            if not line:
-                raise SyntaxError()
-            my_list = line.split(" ")
+    # def do_create(self, line):
+    #     """Usage: create <class> <key 1>=<value 2> <key 2>=<value 2> ...
+    #     Create a new class instance with given keys/values and print its id.
+    #     """
+    #     try:
+    #         if not line:
+    #             raise SyntaxError()
+    #         my_list = line.split(" ")
 
-            kwargs = {}
-            for i in range(1, len(my_list)):
-                key, value = tuple(my_list[i].split("="))
-                if value[0] == '"':
-                    value = value.strip('"').replace("_", " ")
-                else:
-                    try:
-                        value = eval(value)
-                    except (SyntaxError, NameError):
-                        continue
-                kwargs[key] = value
+    #         kwargs = {}
+    #         for i in range(1, len(my_list)):
+    #             key, value = tuple(my_list[i].split("="))
+    #             if value[0] == '"':
+    #                 value = value.strip('"').replace("_", " ")
+    #             else:
+    #                 try:
+    #                     value = eval(value)
+    #                 except (SyntaxError, NameError):
+    #                     continue
+    #             kwargs[key] = value
 
-            if kwargs == {}:
-                obj = eval(my_list[0])()
-            else:
-                obj = eval(my_list[0])(**kwargs)
-                storage.new(obj)
-            print(obj.id)
-            obj.save()
+    #         if kwargs == {}:
+    #             obj = eval(my_list[0])()
+    #         else:
+    #             obj = eval(my_list[0])(**kwargs)
+    #             storage.new(obj)
+    #         print(obj.id)
+    #         obj.save()
 
-        except SyntaxError:
-            print("** class name missing **")
-        except NameError:
-            print("** class doesn't exist **")
+    #     except SyntaxError:
+    #         print("** class name missing **")
+    #     except NameError:
+    #         print("** class doesn't exist **")
 
     def do_show(self, line):
         """Prints the string representation of an instance
