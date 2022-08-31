c06ae5b935314c13143d1cecf3c42ff9c968a146
Sb0009 <phenissia2009@hotmail.fr>
2022-08-31 17:43:16 +0000

    rufi

---
+++ 1-pack_web_static.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""
+Fabric script that generates a .tgz archive from the contents of the
+web_static folder of your AirBnB
+"""
+
+from datetime import datetime
+from fabric.api import local
+from os.path import isdir
+
+
+def do_pack():
+        """that generates a .tgz archive"""
+        try:
+            date = datetime.now().strftime("%Y%m%d%H%M%S")
+            if isdir("versions") is False:
+                local("mkdir versions")
+            filename = "versions/web_static_{}.tgz".format(date)
+            local("tar -cvzf {} web_static".format(filename))
+            return filename
+        except Exception:
+            return None
